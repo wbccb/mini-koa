5982bd83d47c2f40a977b4e0b83b0f2fc6814473
wbccb <wbccb18814122@163.com>
2023-06-20 22:47:36 +0800

    docs: 初始化文档

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "mini-koa2",
+  description: "koa2电子书",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+    ],
+
+    sidebar: [
+      {
+        text: '基础用法',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+        ]
+      },
+      {
+        text: '源码解析',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+        ]
+      },
+      {
+        text: '面试题',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/wbccb/mini-koa2' }
+    ]
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,22 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "mini-koa2"
+  text: "koa2电子书"
+  tagline: My great project tagline
+  actions:
+    - theme: brand
+      text: 正文内容
+      link: /markdown-examples
+
+features:
+  - title: Koa2基础知识总结
+    details: 总结基础的使用方法
+  - title: Koa2源码分析文章
+    details: 通过源码分析，为手写奠定基础
+  - title: Koa2常见的面试题
+    details: 从面试题更加深入理解Koa2
+---
+
